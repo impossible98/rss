88aa358a3bfbde92e1a33c5c43d0eea7ea9bb7f3
Chris <42108459+impossible98@users.noreply.github.com>
2021-10-02 14:44:49 +0800

    feat: add frontend

--- Makefile
+++ Makefile
@@ -10,6 +10,12 @@ build:
 build-docker:
 	@docker build -f ./build/Dockerfile  -t impossible98/rss .
 
+build-frontend:
+	cd frontend; \
+	npm install; \
+	./node_modules/.bin/vue-cli-service build; \
+	cp -r dist/ ../web/public/
+
 dev: \
 	build
 	@./dist/rss
---
+++ frontend/src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </div>
+</template>
